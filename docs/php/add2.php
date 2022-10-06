a6065840bb962ff22e80a14d5e82f62017c33cac
Warren MacEvoy <wmacevoy@gmail.com>
2022-10-06 11:56:46 -0600

    add 2.0 style

--- docs/php/add2.php
+++ docs/php/add2.php
@@ -1,3 +1,24 @@
 <?php 
-var_dump($_REQUEST);
-?>
+$request = json_decode($_REQUEST['request'],true);
+
+if ($request['op'] == "add") {
+    $sum = 0.0;
+    $ok = TRUE;
+    foreach ($request['args'] as $key => $value) {
+        $ok = $ok && preg_match("/^(-?[0-9]+([.][0-9]+)?)$/",$value);
+        error_log("$value is $ok");
+        if ($ok) {
+            $sum += (float) $value;
+        }
+    }
+
+    if ($ok) {
+      $response = array('status' => 'ok', 'value' =>  $sum );
+    } else {
+      $response = array('status' => 'fail', 'message' => 'not all values are numbers');
+    }
+} else {
+    $response = array('status' => 'fail', 'message' => 'unknown op');
+}
+header('Content-Type: application/json; charset=utf-8');
+echo json_encode($response);
